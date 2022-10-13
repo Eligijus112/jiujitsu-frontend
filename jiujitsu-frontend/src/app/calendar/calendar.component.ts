36de2f56b6efa0298b7bf1fb11ab9145c14081bc
eligijus <elasorama@gmail.com>
2022-10-13 10:01:53 +0300

    Added the calendar tab improvements

--- jiujitsu-frontend/src/app/calendar/calendar.component.ts
+++ jiujitsu-frontend/src/app/calendar/calendar.component.ts
@@ -1,25 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Importing the calendar service class
+import CalendarService from './calendar.service';
 
 @Component({
   selector: 'calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
   // Getting the current date
   date = new Date();
+
+  // Defining the user id
+  user_id: number | any;
+
+  // Placeholder for calendar array
+  calendar: any = [];
 
   // Saving only the date part
   // (without the time part)
   today = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate());
 
-  // Creating a sequence of 7 days in the future
-  // (from today to 7 days from today)
-  days = Array.from({length: 7}, (v, k) => k + 1).map(day => {
+  // Placeholder for days
+  days = Array.from({length: 7}, (v, k) => k).map(day => {
     const date = new Date(this.today);
     date.setDate(date.getDate() + day);
-    return date;
-  });
+
+    // Converting to ISOString
+    const isoDate = date.toISOString().split('T')[0];
+
+    // Creating an entry of the following form:
+    // { date: YYYY-mm-dd, weekday: weekday }
+    return {
+      date: isoDate,
+      weekday: date.toLocaleDateString('en-GB', { weekday: 'long' }),
+      morning_count: 0,
+      evening_count: 0
+    };
+  })
+
+  constructor (private calendarService: CalendarService) {}
+
+  ngOnInit() {
+    this.calendarService.autoAuthUser();
+    this.user_id = this.calendarService.getUserId();
+
+    // Querying the calendar
+    this.calendarService.getCalendar(this.days[0].date, this.days[6].date);
+
+    // Subscribing to the calendar
+    this.calendarService.getCalendarObservable().subscribe((data: any) => {
+      this.calendar = data.data;
+
+      // Updating the days array
+      this.days.map(day => {
+        // Getting the date
+        const date = day.date;
+        // Getting the calendar entry of that date
+        const calendar_entry = this.calendar.find((entry: any) => entry.date === date);
+        if (calendar_entry) {
+          day.morning_count = calendar_entry.morning_sum;
+          day.evening_count = calendar_entry.evening_sum;
+        }
+      })
+    });
+  }
 
   totalDays() {
     return this.days.length;
@@ -27,6 +73,14 @@ export class CalendarComponent {
 
   getDates() {
     return this.days;
+  }
+
+  goingMorning(date: string) {
+    this.calendarService.goingToClass(date, this.user_id, 'morning');
+  }
+
+  goingEvening(date: string) {
+    this.calendarService.goingToClass(date, this.user_id, 'evening');
   }
 
 }
---
+++ jiujitsu-frontend/src/app/calendar/calendar.service.ts
@@ -0,0 +1,81 @@
+// Injectable class
+import { Injectable, OnInit } from '@angular/core';
+
+// Importing the HttpClient module
+import { HttpClient } from '@angular/common/http';
+
+// ENV variables
+import { environment } from "../../environments/environment";
+
+// Login service
+import { LoginService } from '../login/login.service';
+
+// Observer object for the calendar
+import { Subject } from 'rxjs';
+
+// Modals
+import Swal from 'sweetalert2';
+
+// Defining the backend URL
+const BACKEND_URL = environment.apiUrl + '/calendar';
+
+@Injectable({providedIn: "root"})
+export class CalendarService {
+
+  calendar = new Subject<[]>();
+
+  constructor(
+    private http: HttpClient,
+    private loginService: LoginService
+    ) { }
+
+  autoAuthUser() {
+    return this.loginService.autoAuthUser();
+  }
+
+  getUserId() {
+    return this.loginService.getUserId();
+  }
+
+  getCalendar(start_date: string, end_date: string) {
+    // Sending the get request
+    return this.http.get(BACKEND_URL, {'params': {'start': start_date, 'end': end_date}}).
+    subscribe((data: any) => {
+      this.calendar.next(data);
+    });
+  }
+
+  getCalendarObservable() {
+    return this.calendar.asObservable();
+  }
+
+  goingToClass(date: string, user_id: number, part_of_day: string) {
+    // Creating the form data for the post request
+    const formData = new FormData();
+
+    // Adding the date and user_id to the form data
+    formData.append('date', date);
+    formData.append('user_id', user_id.toString());
+    formData.append('part_of_day', part_of_day);
+
+    console.log(BACKEND_URL);
+
+    // Sending the post request
+    return this.http.post(BACKEND_URL + '/addEntry', formData)
+    .subscribe((data: any) => {
+      if (data.status_code == 201){
+        Swal.fire({
+          icon: 'success',
+          title: 'Success!',
+          text: 'Successfully added to the calendar',
+          showConfirmButton: false,
+          timer: 3000
+        })
+      }
+    });
+  }
+
+}
+
+// Exporting the class
+export default CalendarService;
